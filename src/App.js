1a8a0c9079e675c4a16225d5f3cc54e94a56da60
miniguiti <gminiguiti@outlook.com>
2020-11-18 19:37:35 -0300

    Removendo arquivos para revisão

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import Student from './pages/Student'
-import './assets/css/style.css'
 
 function App() {
-  const scriptIcons = document.createElement("script")
-  scriptIcons.src = "https://kit.fontawesome.com/9e43a3780f.js"
-  document.body.appendChild(scriptIcons)
-
   return (
-    <Student/>
+    <h2></h2>
   );
 }
 
