b0e90ead099c0181f55b98e93a9a4ee9642a2c73
miniguiti <gminiguiti@outlook.com>
2020-11-23 15:54:06 -0300

    Revert "Removendo arquivos para revisão"
    This reverts commit 1a8a0c9079e675c4a16225d5f3cc54e94a56da60.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Student from './pages/Student'
+import './assets/css/style.css'
 
 function App() {
+  const scriptIcons = document.createElement("script")
+  scriptIcons.src = "https://kit.fontawesome.com/9e43a3780f.js"
+  document.body.appendChild(scriptIcons)
+
   return (
-    <h2></h2>
+    <Student/>
   );
 }
 
